3e8e18eb7743640c9070a146c354f3421d9c0f4d
Natalia Mirnaia <strunailustra@gmail.com>
2019-11-12 23:33:49 +0300

    traffic light

--- Mirnaia2.1.2/ViewController.swift
+++ Mirnaia2.1.2/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var controlButton: UIButton!
+    var currentState = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        redLight.layer.cornerRadius = 75
+        yellowLight.layer.cornerRadius = 75
+        greenLight.layer.cornerRadius = 75
+        controlButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func buttonPressed() {
+        controlButton.setTitle("NEXT", for: .normal)
+        currentState += 1
+        switch currentState % 3 {
+        case 1:
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+        case 2:
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        case 0:
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        default:
+            break
+        }
+        
+    }
 }
 
